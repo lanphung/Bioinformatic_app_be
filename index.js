874dc53f3352dd19e301972cde250f5976b3e9f1
KienTrungz <kien.bt207710@sis.hust.edu.vn>
2023-03-16 16:25:30 +0700

    update port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 const route = require('./src/routes');
 const db = require('./src/config/db');
